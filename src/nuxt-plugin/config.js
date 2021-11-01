5fceb1edb2faac91e9c05147f4397c4d1ec5909e
artem <rubartem@gmail.com>
2021-11-01 15:17:41 +0300

    Add gitignore

--- src/nuxt-plugin/config.js
+++ src/nuxt-plugin/config.js
@@ -3,7 +3,8 @@ export default {
     fb: (url, title, description, image) => `https://www.facebook.com/sharer/sharer.php?u=${url}`,
     vk: (url, title, description, image) => `https://vk.com/share.php?url=${url}&title=${title}%20${description}&image=${image}`,
     tw: (url, title, description, image) => `https://twitter.com/intent/tweet?url=${url}&text=${title}%20${description}`,
-    tg: (url, title, description, image) => `tg://share?url=${url}&text=${title}%20${description}`,
+    tg: (url, title, description, image) => `https://t.me/share/url?url=${url}&text=${title}%20${description}`,
+    tgScheme: (url, title, description, image) => `tg://share?url=${url}&text=${title}%20${description}`,
     ok: (url, title, description, image) => `https://connect.ok.ru/offer?url=${url}&title=${title}&imageUrl=${image}`,
     reddit: (url, title, description, image) => `https://www.reddit.com/submit?url=${url}&title=${title}%20${description}&video_poster_url=${image}`
   }
